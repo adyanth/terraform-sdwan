87dd3ca68a8e4a315f8db5e422c408f71dbb723d
Adyanth H <me@adyanth.dev>
2022-06-14 16:31:43 +0530

    :truck: Modify for change in inputs

--- azure/Provision_Instances/terraform.tf
+++ azure/Provision_Instances/terraform.tf
@@ -1,14 +1,16 @@
-variable "vbond_image" {}
+variable "vbond_uri" {}
 variable "vbond_count" {}
-variable "vmanage_image" {}
+variable "vmanage_uri" {}
 variable "vmanage_count" {}
-variable "vsmart_image" {}
+variable "vsmart_uri" {}
 variable "vsmart_count" {}
 variable "vbond_instances_type" {}
 variable "vmanage_instances_type" {}
 variable "vsmart_instances_type" {}
 variable "username" {}
 variable "password" {}
+variable "org_name" {}
+variable "sp_org_name" {}
 
 
 data "terraform_remote_state" "spam" {
