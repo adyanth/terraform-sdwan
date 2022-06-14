be31b0f7fc62ebf61bf504dab41c3cbcd4cda247
Adyanth H <me@adyanth.dev>
2022-06-14 16:31:16 +0530

    :recycle: :bug: Add features block

--- azure/Provision_Instances/azure.tf
+++ azure/Provision_Instances/azure.tf
@@ -4,8 +4,9 @@ variable "ARM_CLIENT_SECRET" {}
 variable "ARM_TENANT_ID" {}
 
 provider "azurerm" {
-  subscription_id = "${var.ARM_SUBSCRIPTION_ID}"
-  client_id       = "${var.ARM_CLIENT_ID}"
-  client_secret   = "${var.ARM_CLIENT_SECRET}"
-  tenant_id       = "${var.ARM_TENANT_ID}"
+  subscription_id = var.ARM_SUBSCRIPTION_ID
+  client_id       = var.ARM_CLIENT_ID
+  client_secret   = var.ARM_CLIENT_SECRET
+  tenant_id       = var.ARM_TENANT_ID
+  features {}
 }
